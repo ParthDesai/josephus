efa1477a4ecc639af285816711e58f08f6edd3c8
Parth Desai <desaiparth08@gmail.com>
2016-04-03 08:48:30 +0530

    - Adding new test case, and one benchmark

--- lastalive/lastalive_test.go
+++ lastalive/lastalive_test.go
@@ -1,6 +1,7 @@
 package lastalive
 
 import "testing"
+import "math"
 
 // Edge test case
 func TestCalculateLastAliveEdge(t *testing.T) {
@@ -11,6 +12,19 @@ func TestCalculateLastAliveEdge(t *testing.T) {
 	}
 
 	if out != 0 {
+		t.Errorf("Expected: %d, Received: %d", 0, out)
+	}
+}
+
+// Edge Test case 2
+func TestCalculateLastAliveEdge2(t *testing.T) {
+	out, err := CalculateLastAlive(math.MaxUint64)
+
+	if err != nil {
+		t.Errorf("Not Expected any error. But found:" + err.Error())
+	}
+
+	if out != 18446744073709551614 {
 		t.Errorf("Expected: %d, Received: %d", 0, out)
 	}
 }
@@ -35,6 +49,15 @@ func TestCalculateLastAliveNormal(t *testing.T) {
 
 	if out != 6 {
 		t.Errorf("Expected: %d, Received: %d", 0, out)
+	}
+
+}
+
+// Benchmark with maximum value
+func BenchmarkCalculateLastAlive(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		CalculateLastAlive(math.MaxUint64)
 	}
 
 }
